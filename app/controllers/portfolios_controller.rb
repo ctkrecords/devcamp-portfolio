6d5cef345a933720a849c21fdcfb835130381162
developer <ctkrecordings@gmail.com>
2019-07-19 14:34:31 -0600

    ADDED: Implemented show functionality for portfolio module

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -33,6 +33,10 @@ class PortfoliosController < ApplicationController
             format.html { render :edit }
           end
         end
-      end
-      
+    end
+
+    def show
+        @portfolio_item = Portfolio.find(params[:id])
+    end
+
 end
